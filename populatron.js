17e3886f0f36311d9b6b2f89110fa3124631d146
Go Nakano <g.nakano555@gmail.com>
2019-07-17 22:05:26 +0900

    passed test

--- populatron.js
+++ populatron.js
@@ -1,5 +1,17 @@
 const fs = require("fs");
 
 module.exports = {
-  totalPopulation(onFinished) {},
+  totalPopulation(onFinished) {
+    let store = [];
+    const data = fs
+      .readFileSync("cities.csv", "utf-8")
+      .split(",")
+      .slice(8);
+    for (let i = 0; i < data.length; i++) {
+      if (i % 6 === 0) {
+        store.push(Number(data[i]));
+      }
+    }
+    onFinished(store.reduce((acc, cur) => (acc += cur)));
+  },
 };
